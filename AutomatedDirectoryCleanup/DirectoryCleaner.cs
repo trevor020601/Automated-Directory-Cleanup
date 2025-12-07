306e63bf5c2ca1dbe76ec19ae5a78607c3472982
trevor020601 <trevorkarl5@gmail.com>
2025-12-07 13:41:11 -0600

    Skip locked files, skip files that cause other exceptions, and logging

--- AutomatedDirectoryCleanup/DirectoryCleaner.cs
+++ AutomatedDirectoryCleanup/DirectoryCleaner.cs
@@ -1,24 +1,41 @@
-﻿namespace AutomatedDirectoryCleanup;
+﻿using Microsoft.Extensions.Logging;
 
-public class DirectoryCleaner
+namespace AutomatedDirectoryCleanup;
+
+public class DirectoryCleaner(ILogger<DirectoryCleaner> logger)
 {
     public void DeleteOldFilesByExtension(CleanupDirectory cleanupDirectory)
     {
-        if (cleanupDirectory.DirectoryPath is null)
+        if (!Directory.Exists(cleanupDirectory.DirectoryPath))
         {
-            throw new ArgumentNullException(cleanupDirectory.DirectoryPath);
+            throw new DirectoryNotFoundException(cleanupDirectory.DirectoryPath);
         }
 
         var files = new FileSystem().GetFiles(cleanupDirectory.DirectoryPath);
-        if (files.Length < 0)
-        {
-            return;
-        }
-
         var filesToDelete = files.Where(f => cleanupDirectory.Extensions.Contains(f.Extension[1..]))
             .Where(f => f.CreationTime < DateTime.Now.AddDays(-cleanupDirectory.AgeTimeSpan.Days))
             .ToList();
 
-        filesToDelete.ForEach(f => f.Delete());
+        foreach (var file in filesToDelete)
+        {
+            try
+            {
+                var isLocked = FileInfoExtensions.IsFileLocked(file);
+                if (!isLocked)
+                {
+                    logger.LogInformation("{Name} has been deleted.", file.Name);
+                    file.Delete();
+                }
+                else
+                {
+                    logger.LogInformation("{Name} is locked. Skipping...", file.Name);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "{Name} has been skipped due to exception: {Message}", file.Name, ex.Message);
+                continue;
+            }
+        }
     }
 }
