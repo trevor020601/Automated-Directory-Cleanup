0b67485d5c9e75f35e9452fb6eb9d6746f628ed1
trevor020601 <trevorkarl5@gmail.com>
2025-12-13 16:33:04 -0600

    Added Polly resilience pipeline with retry strategy for potentially locked files; Keep researching Polly

--- AutomatedDirectoryCleanup/AutomatedDirectoryCleanup/DirectoryCleaner.cs
+++ AutomatedDirectoryCleanup/AutomatedDirectoryCleanup/DirectoryCleaner.cs
@@ -1,9 +1,57 @@
 ﻿using Microsoft.Extensions.Logging;
+using Polly;
 
 namespace AutomatedDirectoryCleanup;
 
 public class DirectoryCleaner(ILogger<DirectoryCleaner> logger)
 {
+    private const int _errorCodeBits = 0x0000FFFF;
+    // https://learn.microsoft.com/en-us/dotnet/standard/io/handling-io-errors#handling-ioexception
+    private const int _errorCodeSharingViolation = 32;
+
+    public void DeleteOldFilesByExtensionPolly(CleanupDirectory cleanupDirectory)
+    {
+        if (!Directory.Exists(cleanupDirectory.DirectoryPath))
+        {
+            throw new DirectoryNotFoundException(cleanupDirectory.DirectoryPath);
+        }
+
+        var files = new FileSystem().GetFiles(cleanupDirectory.DirectoryPath);
+        var filesToDelete = files.Where(f => cleanupDirectory.Extensions.Contains(f.Extension[1..]))
+            .Where(f => f.CreationTime < DateTime.Now.AddDays(-cleanupDirectory.AgeTimeSpan.Days))
+            .ToList();
+
+        var retryStrategyOptions = new Polly.Retry.RetryStrategyOptions()
+        {
+            ShouldHandle = new PredicateBuilder().Handle<IOException>(ex => (ex.HResult & _errorCodeBits) == _errorCodeSharingViolation),
+            MaxRetryAttempts = 3,
+            Delay = TimeSpan.FromSeconds(5),
+            OnRetry = args =>
+            {
+                logger.LogInformation("Retrying to delete file, Attempt: {0}", args.AttemptNumber);
+                return default;
+            }
+        };
+
+        var pipelineBuilder = new ResiliencePipelineBuilder().AddRetry(retryStrategyOptions).Build();
+
+        foreach (var file in filesToDelete)
+        {
+            try
+            {
+                pipelineBuilder.Execute(() => {
+                    file.Delete();
+                    logger.LogInformation("{Name} has been deleted.", file.Name);
+                });
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "{Name} has been skipped due to exception: {Message}", file.Name, ex.Message);
+                continue;
+            }
+        }
+    }
+
     public void DeleteOldFilesByExtension(CleanupDirectory cleanupDirectory)
     {
         if (!Directory.Exists(cleanupDirectory.DirectoryPath))
@@ -23,8 +71,8 @@ public class DirectoryCleaner(ILogger<DirectoryCleaner> logger)
                 var isLocked = FileInfoExtensions.IsFileLocked(file);
                 if (!isLocked)
                 {
-                    logger.LogInformation("{Name} has been deleted.", file.Name);
                     file.Delete();
+                    logger.LogInformation("{Name} has been deleted.", file.Name);
                 }
                 else
                 {
